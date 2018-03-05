aaa1b362be4bbb3ddfd2feb6ce763870195d863c
Marek NECESANY <marek.necesany@smile.fr>
2018-03-05 19:06:16 +0100

    init interact.js

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -12,6 +12,6 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('PdfLayoutGenBundle:Default:index.html.twig');
+        return $this->render('@PdfLayoutGen/Default/index.html.twig');
     }
 }
