92a3cb90668538d7da2853c4a0b81ade70e56213
Marek NECESANY <marek.necesany@smile.fr>
2018-07-04 18:48:07 +0200

    project update

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,7 +16,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Ramik\PdfLayoutGenBundle\PdfLayoutGenBundle(),
+            new Raneomik\PdfLayoutGenBundle\PdfLayoutGenBundle(),
+            new \AppBundle\AppBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
